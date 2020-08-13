d6891f5c3522a8241e6092b0538d687c0cf1036f
Yohansky99 <69236028+yohansky@users.noreply.github.com>
2020-08-13 12:25:29 +0700

    Bab 8.Concurrent Programming (kelas Thread)

--- ThreadSaya.java
+++ ThreadSaya.java
@@ -1,36 +1,41 @@
 package Concurrent_Programming;
 
-public class ThreadSaya implements Runnable {
+public class ThreadSaya extends Thread {
     private Thread thread;
     private String namaThread;
 
     public ThreadSaya(String namaThread) {
         this.namaThread = namaThread;
-        System.out.println("Thread " + namaThread + " diciptakan");
+        System.out.println("thread " + namaThread + " diciptakan.");
     }
 
-    @Override
     public void run() {
-        System.out.println("Thread " + namaThread + " dijalankan");
+        System.out.println(namaThread + " dijalankan.");
 
         try {
             for (int i = 10; i > 0; i--) {
                 System.out.println("Thread " + namaThread + " ke-" + i);
-
-                Thread.sleep(50);
             }
-        } catch (InterruptedException e) {
+            Thread.sleep(50);
+            } catch (InterruptedException e) {
             System.out.println("Thread " + namaThread + " diinterupsi.");
-        }
-
+            }
         System.out.println("Thread " + namaThread + " selesai.");
     }
-
     public void start() {
-        System.out.println("Thread " + namaThread + " dimulai.");
+        System.out.println("Thread " + namaThread + " dimulai");
 
-        if (thread == null) { // belum diinstansiasi
+        if (thread == null) {
             thread = new Thread(this, namaThread);
+            thread.start();
+        }
+    }
+    public void start(int priority) {
+        System.out.println("Thread " + namaThread + " dimulai");
+
+        if (thread == null) {
+            thread = new Thread(this, namaThread);
+            thread.setPriority(priority);
             thread.start();
         }
     }
